86a2839af90732f8e349d732e4218ebfcbd53174
Silvio Carreiro <59662967+silviocarreiro@users.noreply.github.com>
2024-05-20 18:21:43 -0300

    projeto atualizado
    arquivos para curso 2

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index, name = 'index'),
-    path('imagem/', imagem, name = 'imagem'),
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 ]
