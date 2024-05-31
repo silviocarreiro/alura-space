8a4d90a843976a73717b1ad213375bf247d2ba59
Silvio Carreiro <59662967+silviocarreiro@users.noreply.github.com>
2024-05-31 10:57:57 -0300

    atualizacao model fotografia

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from galeria.models import Fotografia
+
+class ListandoFotografia(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+
+admin.site.register(Fotografia, ListandoFotografia)
 
 # Register your models here.
